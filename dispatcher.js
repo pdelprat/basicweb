c7d06da3e7384cb22e8c3da11f83eb75b1674073
Pascal Delprat <pascal.delprat@online.fr>
2021-02-09 08:57:38 +0100

    Clean console messages

--- dispatcher.js
+++ dispatcher.js
@@ -2,7 +2,7 @@ const EventEmitter = require('events');
 
 class Dispatcher extends EventEmitter {
   send(eventName, data) {
-    console.log('Dispather receive: ', data);
+    console.log('Dispather receive:', data);
 
     this.emit(eventName, data);
   }
